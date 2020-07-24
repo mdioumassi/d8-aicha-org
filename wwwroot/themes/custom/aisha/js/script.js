7f15d2bcc7c1cc41c9d64b57ffadc1b5242d86f0
user.name <mdioumassi@yahoo.fr>
2020-07-25 01:43:41 +0200

    first commit

--- wwwroot/themes/custom/aisha/js/script.js
+++ wwwroot/themes/custom/aisha/js/script.js
@@ -1,6 +1,6 @@
 (function($, D){
   $(document).ready(function(){
-    $("body").addClass("w3-pale-green");
+   // $("body").addClass("w3-pale-green");
 
     $("#btn-play span").click(function () {
       var href = $("#btn-play a").attr('href');
